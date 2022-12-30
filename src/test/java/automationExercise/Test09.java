50ad1fda291d3b32e73e93a13a1ebd6559222afa
isa Siypak <siypaki@gmail.com>
2022-12-30 17:07:16 +0300

    30/12/2022

--- src/test/java/automationExercise/Test09.java
+++ src/test/java/automationExercise/Test09.java
@@ -1,6 +1,7 @@
 package automationExercise;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -22,24 +24,23 @@ public class Test09 {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //   1. Launch browser
         driver.get("http://automationexercise.com");
-
-
-
     }
 
-    //   1. Launch browser
-//2. Navigate to url 'http://automationexercise.com'
+    //2. Navigate to url 'http://automationexercise.com'
     @Test
     public void test1() throws InterruptedException {
 
 
-        //            3. Verify that home page is visible successfully
+        //   3. Verify that home page is visible successfully
         WebElement homePage = driver.findElement(By.xpath("//*[@lang='en']"));
         Assert.assertTrue(homePage.isDisplayed());
 
-        //4. Click on 'Products' button
+        // driver.switchTo().alert().dismiss();
 
+        //4. Click on 'Products' button
+        driver.navigate().to("https://automationexercise.com/products");
         WebElement products = driver.findElement(By.xpath("//*[@href='/products']"));
         products.click();
         //5. Verify user is navigated to ALL PRODUCTS page successfully
@@ -47,16 +48,23 @@ public class Test09 {
         Assert.assertTrue(allProducts.isEnabled());
         //6. Enter product name in search input and click search button
         WebElement searchProduct = driver.findElement(By.xpath("//input[@id='search_product']"));
-        searchProduct.sendKeys("POLO", Keys.ENTER);
+        searchProduct.sendKeys("POLO");
         WebElement button = driver.findElement(By.xpath("//*[@type='button']"));
+        button.click();
 
         //7. Verify 'SEARCHED PRODUCTS' is visible
         WebElement searchedProduct = driver.findElement(By.xpath("//div[@class='features_items']"));
         Assert.assertTrue(searchedProduct.isDisplayed());
 
         //8. Verify all the products related to search are visible
+        WebElement searchedAllProducts = driver.findElement(By.xpath("//div[@class='features_items']"));
+        Assert.assertTrue(searchedAllProducts.isDisplayed());
     }
 
+    @After
+    public void tearDown() throws Exception {
+        driver.close();
+    }
 }
 
 
